b8b7b83fa61e0a36534fc48743dc8e26834d80c2
Skiv <just.skiv@gmail.com>
2011-06-27 07:54:59 +0600

    commit

--- Controller.java
+++ Controller.java
@@ -1,3 +1,6 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * Created by IntelliJ IDEA.
  * User: skiv
@@ -5,8 +8,13 @@
  * Time: 4:26
  * To change this template use File | Settings | File Templates.
  */
-public class Controller {
+public class Controller implements ActionListener{
   public String getTest(String str){
     return str + " test";
+  }
+
+  public void actionPerformed(ActionEvent actionEvent) {
+    String s = actionEvent.toString();
+    System.out.println("asd: " + s);
   }
 }
--- View.java
+++ View.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by IntelliJ IDEA.
@@ -7,20 +10,33 @@ import javax.swing.*;
  * Time: 4:23
  * To change this template use File | Settings | File Templates.
  */
-public class View extends JFrame{
+public class View extends JFrame implements ActionListener{
+  String field_x = "X";
+  String field_o = "O";
+  String field_n = ".";
   public View() {
     Controller controller = new Controller();
     System.out.println(controller.getTest("Skiv"));
 
 
     JPanel panel = new JPanel();
-    JButton b1 = new JButton("test");
+    panel.setLayout(new GridLayout(3, 1));
+    JButton[] b = new JButton[9];
 
     add(panel);
-
-    panel.add(b1);
+    for (int i = 0; i < 9; i++) {
+      b[i] = new JButton(field_n);
+      b[i].addActionListener(this);
+      b[i].setActionCommand("Command :" + Integer.toString(i));
+      panel.add(b[i]);
+    }
     panel.setVisible(true);
     setVisible(true);
     pack();
+  }
+
+  public void actionPerformed(ActionEvent actionEvent) {
+    String str = actionEvent.getSource().toString();
+    System.out.println(str);
   }
 }
